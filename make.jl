919dcc94ff2458529e784e15b0abd174f5549657
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-04-19 21:51:42 -0400

    jekyll that mf

--- make.jl
+++ make.jl
@@ -1,13 +1,7 @@
 using Weave
-using Mustache
 
 sources = filter(endswith("Jmd"), joinpath.("sources", readdir("sources")))
 
-tmpl = read("slides.tpl")
-
 for source in sources
-    # Step 1 - generate a raw md file from the Jmd file
-    weave(source; out_path = replace(source, "sources" => "out"), informat = "markdown", doctype = "github")
-    # Step 2 - generate the html from the md file
-    render()
+    weave(source; out_path = replace(source, "sources" => "docs"), informat = "markdown", doctype = "github")
 end
